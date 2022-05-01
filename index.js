2006c29ddef03ec0dfe38eebd87db55704adaf5a
NikitaRagen <evgpool@yandex.ru>
2022-05-01 16:43:40 +0500

    refactor: remove unnecessary loading

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const routes = {
   '/statistics': {
-    handler: () => import('/src/statistics.js'),
+    importer: () => import('/src/statistics.js'),
     title: 'Рейтинг | Tic Tac Toe',
     styleSheet: '/assets/css/statistics.css'
   },
   '/history': {
-    handler: () => import('/src/history.js'),
+    importer: () => import('/src/history.js'),
     title: 'История игр | Tic Tac Toe',
     styleSheet: '/assets/css/history.css'
   }
 };
 
 const handleRoute = async(location) => {
-  const {handler, title, styleSheet} = routes[location];
-  const {default: pageLoader} = await handler();
+  const {importer, title, styleSheet} = routes[location];
+  if (!document.head.querySelector(`link[href="${styleSheet}"]`)) {
+    const cssLink = document.createElement('link');
+
+    cssLink.rel = 'stylesheet';
+    cssLink.type = 'text/css';
+    cssLink.href = styleSheet;
+
+    document.head.append(cssLink);
+  }
+
+  document.querySelector('.container').innerHTML = '';
+  document.head.querySelector('title').textContent = title;
+
+  if (!routes[location].pageLoader) {
+    const {default: loader} = await importer();
+
+    routes[location].pageLoader = loader;
+  }
+
+  const {pageLoader} = routes[location];
   const page = await pageLoader(async(newLocation) => {
     await handleRoute(newLocation);
 
     window.history.pushState({}, '', newLocation);
   });
-  const cssLink = document.createElement('link');
 
-  cssLink.rel = 'stylesheet';
-  cssLink.type = 'text/css';
-  cssLink.href = styleSheet;
-  document.querySelector('.container').innerHTML = '';
-
-  document.head.append(cssLink);
-  document.head.querySelector('title').textContent = title;
   document.querySelector('.container').append(page);
 };
 
