5662b0e3152b7f4906298096b82cdabc93a8de6f
NikitaRagen <evgpool@yandex.ru>
2022-07-13 20:13:24 +0500

    refactor: groom code

--- src/components/chess/model/chesspiece.js
+++ src/components/chess/model/chesspiece.js
@@ -1,38 +1,34 @@
-
 class ChessPiece {
-    constructor(name, isAttacked, color, id) {
-        this.name = name // string
-        this.isAttacked = isAttacked // boolean
-        this.color = color // string
-        this.id = id // string
+  constructor(name, isAttacked, color, id) {
+    this.name = name;
+    this.isAttacked = isAttacked;
+    this.color = color;
+    this.id = id;
+  }
+
+  setSquare(newSquare) {
+    if (newSquare === undefined) {
+      this.squareThisPieceIsOn = newSquare;
+
+      return;
     }
 
-    setSquare(newSquare) {
-        // set the square this piece is sitting top of. 
-        // on any given piece (on the board), there will always be a piece on top of it. 
-        // console.log(newSquare)
-        if (newSquare === undefined) {
-            this.squareThisPieceIsOn = newSquare
-            return 
-        }
-
-        if (this.squareThisPieceIsOn === undefined) {
-            this.squareThisPieceIsOn = newSquare
-            newSquare.setPiece(this)
-        }
-
-        const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisPieceIsOn.y != newSquare.y
-
-        if (isNewSquareDifferent) {
-            // console.log("set")
-            this.squareThisPieceIsOn = newSquare
-            newSquare.setPiece(this)
-        }
+    if (this.squareThisPieceIsOn === undefined) {
+      this.squareThisPieceIsOn = newSquare;
+      newSquare.setPiece(this);
     }
 
-    getSquare() {
-        return this.squareThisPieceIsOn
+    const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisPieceIsOn.y != newSquare.y;
+
+    if (isNewSquareDifferent) {
+      this.squareThisPieceIsOn = newSquare;
+      newSquare.setPiece(this);
     }
+  }
+
+  getSquare() {
+    return this.squareThisPieceIsOn;
+  }
 }
 
 
